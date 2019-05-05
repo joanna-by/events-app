4b647dc223a780926b84d992942386e8742c8464
joanna-by <j.bykowska@gmail.com>
2019-05-05 13:52:09 +0200

    Add css styles

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { Container } from "semantic-ui-react";
 import EventDashboard from "../../features/event/eventDashboard/EventDashboard";
 import NavBar from "../../features/event/nav/navBar/NavBar";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <NavBar />
-      <EventDashboard />
+      <Container className="main">
+        <EventDashboard />
+      </Container>
     </div>
   );
 }
