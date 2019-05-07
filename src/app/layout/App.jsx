63c09ccc06822a5aa27f6bc2b26545f85b46466f
joanna-by <j.bykowska@gmail.com>
2019-05-07 08:43:52 +0200

    Install react-router-dom and configure routes

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 import { Container } from "semantic-ui-react";
+import { Route } from "react-router-dom";
 import EventDashboard from "../../features/event/eventDashboard/EventDashboard";
-import NavBar from "../../features/event/nav/navBar/NavBar";
+import NavBar from "../../features/nav/navBar/NavBar";
+import PeopleDashboard from "../../features/user/peopleDashboard/PeopleDashboard";
+import UserDetailedPage from "../../features/user/userDetailed/UserDetailedPage";
+import SettingsDashboard from "../../features/user/settings/SettingsDashboard";
+import EventForm from "../../features/event/eventForm/EventForm";
+import EventDetailedPage from "../../features/event/eventDetailed/EventDetailedPage"
 
 function App() {
   return (
     <div>
       <NavBar />
       <Container className="main">
-        <EventDashboard />
+        <Route path="/" component={EventDashboard} />
+        <Route path="/events" component={EventDashboard} />
+        <Route path="/event/:id" component={EventDetailedPage} />
+        <Route path="/people" component={PeopleDashboard} />
+        <Route path="/profile/:id" component={UserDetailedPage} />
+        <Route path="/settings" component={SettingsDashboard} />
+        <Route path="/createEvent" component={EventForm} />
       </Container>
     </div>
   );
